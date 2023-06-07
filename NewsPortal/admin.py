d533bd9c57b391cb99bbcd3b156e991d56cb72c1
AlexDawg <130207177+AlexDawg@users.noreply.github.com>
2023-06-07 16:31:18 +0700

    Delete NewsPortal directory

--- NewsPortal/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Post, PostCategory, Category, CategorySubscribe, Author
-
-admin.site.register(Post)
-admin.site.register(Category)
-# Register your models here.
